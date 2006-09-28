aecefc5e5b81a3e00e2254a3bdf5880c60e66461
Joachim Breitner <mail@joachim-breitner.de>
2006-09-28 21:40:12 +0000

    cgi anfÃ¤nge

--- cgi-edit.py
+++ cgi-edit.py
@@ -1,7 +1,74 @@
 #!/usr/bin/python
 
 import cgi
+import os
 import cgitb; cgitb.enable()
 
+def main ():
+	basename = os.environ.get('PATH_INFO','/')[1:]
 
-cgi.test()
+	if exists(basename):
+		# edit
+		pass
+	else:
+		# new page
+		pass
+
+
+	form = cgi.FieldStorage()
+
+
+	print_headers()
+	print_page()
+
+
+def exists(basename):
+	return True
+
+def print_headers():
+	print "Content-type: text/html"
+	print
+
+def print_page(new=True, basename=None, ext=None, content=None, error=None):
+	if new:
+		if basename:
+			title = "Creating new page \"%s\"" % basename
+			nameform = cgi.encode(basename)
+		else:
+			title = "Creating new page"
+			nameform = '<input name="basename" type="text"/>'
+
+		pageform = '''
+			<h3>Page Name </h3>
+			%(nameform)s
+			<h3>Page Type</h3>
+			<input type="radio" name="type" value="!wiki">Wiki page</input>
+			<input type="radio" name="type" value="tex">LaTeX page</input>
+			<input type="radio" name="type" value="!other">Other, specify extension:
+			<input type="text" name="ext" /> '''%{'nameform':nameform}
+	else:
+		title = "Editing page \"%s\"" % basename
+		pageform = '<h3>%(basename)s, a %(type)s' % { 'basename':cgi.enc(basename), 'type':'?'}
+	
+
+	form = '''
+	<form action="#">
+	%(pageform)s
+	<h3>Content</h3>
+	<textarea name="content">
+	%(content)s
+	</textarea>
+	</form>''' % { 'pageform':pageform, 'content': ''}
+	print '''
+<html>
+<head>
+<title>%(title)s</title>
+</head>
+<boddy>
+<h1>%(title)s</h1>
+%(form)s
+</body>
+</html>''' % { 'title': cgi.escape(title), 'form': form}
+
+
+main()
