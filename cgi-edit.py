21d22a430b1090b8d4bf4dae668b1982222cf9f1
Joachim Breitner <mail@joachim-breitner.de>
2006-09-30 22:48:48 +0000

    nicer and more verbose

--- cgi-edit.py
+++ cgi-edit.py
@@ -210,7 +210,8 @@ def print_page(new, basename, ext, content, log, rev, conf_rev, error):
 	<textarea name="content" cols="80" rows="30">%(content)s</textarea>
 	<input type="hidden" name="revision" value="%(rev)i"/>
 	<h3>Commit log entry</h3>
-	<input type="text" name="comment" cols="80" value="%(comment)s"/> (compulsory)
+	Please describe your changes. This is mandatory.<br/>
+	<input type="text" name="comment" size="80" value="%(comment)s"/>
 	<h3>Commit changes</h3>
 	%(conftext)s
 	<button type="submit">Commit changes
